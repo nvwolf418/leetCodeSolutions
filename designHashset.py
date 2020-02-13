0b09d0e0f4596ef22dedc794168386e3600d0727
nvwolf418 <taispringer@gmail.com>
2020-02-13 16:13:51 -0600

    create designHashset.py
    Design hashset, which is an unordered collection of keys

---
+++ designHashset.py
@@ -0,0 +1,21 @@
+class MyHashSet:
+
+    def __init__(self):
+        self.data = []
+
+    def add(self, key: int) -> None:
+        if key not in self.data:
+            self.data.append(key)
+
+    def remove(self, key: int) -> None:
+        if key in self.data:
+            self.data.remove(key)
+
+    def contains(self, key: int) -> bool:
+        """
+        Returns true if this set contains the specified element
+        """
+        if key in self.data:
+            return True
+        return False
+
